01b143a4dabdbc2a4ba13ca2017aa86271984a65
shenalnimsara1234-ai <shenalnimsara1234@gmail.com>
2026-04-01 23:03:35 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ============================================
 // 🔒 FIXED CREDENTIALS — Change these values!
 // ============================================
-const FIXED_USERNAME = "Kavindu";      // Change to your lover's name
+const FIXED_USERNAME = "kavindu";      // Change to your lover's name
 const FIXED_PASSWORD = "iloveyou";          // Change to your secret word
 
 // ============================================
